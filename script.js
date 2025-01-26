1a5a7c2d3dc78e0c6438cb2ba1d3613d43f9b1d8
MarcosPauloDosSantosSilva <mpssss@hotmail.com>
2025-01-26 18:40:26 -0300

    Projeto Java Web SpringBoot+FrontEnd 2025

---
+++ script.js
@@ -0,0 +1,53 @@
+const formulario = document.querySelector("form");
+const inpultnome = document.querySelector(".nome");
+const inpultemail = document.querySelector(".email");
+const inpultsenha = document.querySelector(".senha");
+const inpulttelefone = document.querySelector(".fone");
+
+
+
+
+
+function Cadastrar(){
+
+    fetch("http://localhost:8080/Cadastrar",
+        {
+            headers:{
+                'Accept':'application/json',
+                'Content-Type':'application/json'
+            },
+            method:"POST",
+            body:JSON.stringify({
+                nome: inpultnome.value,
+                email: inpultemail.value,
+                senha: inpultsenha.value,
+                fone: inpulttelefone.value,})
+        }
+    )
+
+    .then(function(res){console.log(res)})
+    .catch(function(res){console.log(res)})
+
+}
+
+function limparInpults(){
+
+    inpultnome.value = "";
+    inpultemail.value = "";
+    inpultsenha.value = "";
+    inpulttelefone.value = "";
+
+};
+
+formulario.addEventListener("submit", function(event){
+    event.preventDefault();
+    
+
+    Cadastrar();
+    limparInpults();
+
+   
+})
+
+
+    
